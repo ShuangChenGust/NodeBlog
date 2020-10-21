241058401938b59caab443b243e6cacf494161b0
shuangchen <guschenwpi@gmail.com>
2020-10-20 20:21:52 -0400

    fix the problem of forEach, non iterable string

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 //set the view enginee
 app.set('view engine', 'ejs')
 
+//Set the router folder;
+app.use('/articles', articleRouter)
+
 //set route
 app.get('/', (req, res)=>{
-    res.render("index");
+    const articles = [{
+        title: 'Test Article',
+        createdAt: Date.now(),
+        description: 'Test Description'
+    },
+    {
+        title: 'Test Article2',
+        createdAt: Date.now(),
+        description: 'Test Description2'
+    }]
+    res.render('index', { articles: 'articles' })
 })
 
 //set port
