711a0d245d8fac29c2b4ff1db72f1276fc658fa6
shuangchen <guschenwpi@gmail.com>
2020-10-29 17:53:35 -0400

    configure the MongoDB

--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,27 @@ router.get('/', (req, res)=>{
     res.send('in Articles')
 })
 
+router.get('/new', (req, res)=>{
+    res.render('articles/new')
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', async (req, res) =>{
+    const article = new article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try{
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch(e){
+        res.render('articles/new', { article: article })
+    }
+    
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+//connect to database
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 //set the view enginee
 app.set('view engine', 'ejs')
 
-//Set the router folder;
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended:false}))
+
+
 
 //set route
 app.get('/', (req, res)=>{
     const articles = [{
         title: 'Test Article',
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description: 'Test Description'
     },
     {
         title: 'Test Article2',
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description: 'Test Description2'
     }]
-    res.render('index', { articles: articles })
+    res.render('articles/index', { articles: articles })
 })
+
+//Set the router folder;
+app.use('/articles', articleRouter)
 
 //set port
 app.listen(5000)
